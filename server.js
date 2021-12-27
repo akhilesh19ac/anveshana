77370d613d199bf8d1ccf911da016f7961708d1a
M Akhilesha Acharya <m.akhilesha.acharya@sap.com>
2021-12-27 21:28:05 +0530

    heroku changes

--- server.js
+++ server.js
@@ -12,18 +12,19 @@ app.get('/api/users', (req,res)=>{
     res.json("hey");
 });
 
-app.get('/', (req,res)=>{
-    console.log("user home page");
-});
+// app.get('/', (req,res)=>{
+//     console.log("user home page");
+// });
 
 app.listen(process.env.PORT || port, ()=>{
     console.log(`Server running in port:  ${port}`);
 });
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(process.cwd()+'/dist'));
 
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get('/*',function(req,res){
+    console.log("test inside slash entry in nodejs file" +(process.cwd()+'/dist/sample-proj/index.html'))
+    res.sendFile(process.cwd()+'/dist/sample-proj/index.html');
 });
 
 
